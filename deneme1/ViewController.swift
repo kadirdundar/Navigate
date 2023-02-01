7bd2220d24c391ed5953bde11635ea748da19eff
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2023-02-01 19:32:42 +0300

    first commit

--- deneme1/ViewController.swift
+++ deneme1/ViewController.swift
@@ -1,19 +1,76 @@
-//
-//  ViewController.swift
-//  deneme1
-//
-//  Created by Kadir Dündar on 30.01.2023.
-//
-
+import MapKit
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate , CLLocationManagerDelegate {
+    
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.requestLocation()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        mapView.delegate = self
+        
+        
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+        
+    }
+        
+    func mapThis(destinationCord : CLLocationCoordinate2D){
+        
+        let sourceCoordinate = (locationManager.location?.coordinate)!
+        
+        let sourcePlaceMark = MKPlacemark(coordinate: sourceCoordinate)
+        let destPlaceMark = MKPlacemark(coordinate: destinationCord)
+        
+        let sourceItem = MKMapItem(placemark: sourcePlaceMark)
+        let destItem = MKMapItem(placemark: destPlaceMark)
+        
+        let destinationRequest = MKDirections.Request()
+        destinationRequest.source = sourceItem
+        destinationRequest.destination = destItem
+        destinationRequest.transportType = .automobile
+        destinationRequest.requestsAlternateRoutes = true
+        
+        let directions = MKDirections(request: destinationRequest)
+        directions.calculate { response, error in
+            
+            guard let response = response else {
+                
+                if let error = error{
+                    print("something is wrong")
+                }
+                          return
+            }
+            let route = response.routes[0]
+            self.mapView.addOverlay(route.polyline)
+            self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+        }
+        
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let render = MKPolylineRenderer(overlay: overlay as! MKOverlay)
+        
+        render.strokeColor = .blue
+        return render
+    }
+    
 }
+
+
+  
 
